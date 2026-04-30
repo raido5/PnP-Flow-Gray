db20eefe670dbb5376a924b4f4d20889444de829
raido <raid.taha@epfl.ch>
2026-04-30 11:20:23 +0200

    Prepare the model for real [0,1] parametrization

--- pnpflow/utils.py
+++ pnpflow/utils.py
@@ -571,9 +571,14 @@ def postprocess(img, args):
         if args.dataset == "afhq_cat":
             img = (img + 1) / 2
         elif args.num_channels == 1:  # grayscale
-            invTrans = v2.Normalize(
-            mean=[-0.5 / 0.5], std=[1./0.5])
-            img = invTrans(img)
+            normalization = getattr(args, 'normalization', 'minus1_plus1')
+            if normalization == 'minus1_plus1':
+                invTrans = v2.Normalize(mean=[-0.5 / 0.5], std=[1. / 0.5])
+                img = invTrans(img)
+            elif normalization == '0_1':
+                pass
+            elif normalization == '0_255':
+                img = img / 255.0
         else:
             invTrans = v2.Normalize(
                 mean=[-0.5 / 0.5, -0.5 / 0.5, -0.5 / 0.5], std=[1./0.5, 1./0.5, 1./0.5])
